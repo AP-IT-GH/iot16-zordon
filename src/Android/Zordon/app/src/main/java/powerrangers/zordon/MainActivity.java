bf0f23abffe7c7bfa069865a19c9128d623b48f5
KennyGuldentops <kenny_guldentops@msn.com>
2016-10-03 14:26:53 +0200

    Simpele versie van Speech to tekst API geïntegreerd (reverted from commit 51cee5d28cad842386d51feff47acc13973a59bd)

--- src/Android/Zordon/app/src/main/java/powerrangers/zordon/MainActivity.java
+++ src/Android/Zordon/app/src/main/java/powerrangers/zordon/MainActivity.java
@@ -2,12 +2,61 @@ package powerrangers.zordon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Locale;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected static final int RESULT_SPEECH = 1;
+    private Button PraatButton;
+    private TextView GesprokenZin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GesprokenZin = (TextView) findViewById(R.id.GesprokenZin);
+        PraatButton = (Button) findViewById(R.id.PraatButton);
+
+        PraatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(
+                        RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, Locale.getDefault());
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    GesprokenZin.setText("");
+                } catch (ActivityNotFoundException a) {
+                    Toast.makeText(getApplicationContext(),
+                            "Your device doesn't support Speech to Text",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_SPEECH: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> text = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    GesprokenZin.setText(text.get(0));
+                }
+                break;
+            }
+        }
     }
 }

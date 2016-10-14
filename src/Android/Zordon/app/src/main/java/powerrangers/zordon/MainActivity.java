17f0bf856b23842582e6df71ae710c1387047e7e
Gilli95 <Gilli95@live.com>
2016-10-14 15:48:48 +0200

    MQTT connect en send message voor android. Same met kenny geschreven

--- src/Android/Zordon/app/src/main/java/powerrangers/zordon/MainActivity.java
+++ src/Android/Zordon/app/src/main/java/powerrangers/zordon/MainActivity.java
@@ -2,6 +2,12 @@ package powerrangers.zordon;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import java.util.ArrayList;
 import java.util.Locale;
 import android.app.Activity;
@@ -14,8 +20,25 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+import static android.R.attr.content;
 
 public class MainActivity extends AppCompatActivity {
+
+    String username = "xnkcayag";
+    String password = "DtCGtuL2kVfk";
+    String server = "ssl://m21.cloudmqtt.com:22452";
+    String clientId = MqttClient.generateClientId();
+    public MqttAndroidClient client;
 
     protected static final int RESULT_SPEECH = 1;
     private Button PraatButton;
@@ -25,8 +48,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnSendMsg = (Button) findViewById(R.id.SendMessage);
         GesprokenZin = (TextView) findViewById(R.id.GesprokenZin);
         PraatButton = (Button) findViewById(R.id.PraatButton);
+
+        client = new MqttAndroidClient(this.getApplicationContext(), server,clientId);
+        try {
+            MqttConnectOptions options = new MqttConnectOptions();
+
+            options.setUserName(username);
+            options.setPassword(password.toCharArray());
+
+            IMqttToken token = client.connect(options);
+
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Log.i("f", "GElukt");
+                }
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Log.i("d", "GEfailed");
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+        if (client.isConnected() == true){
+            TextView statusTxt = (TextView) findViewById(R.id.StatusLabel);
+            statusTxt.setText("Status: Disconnected");
+            Log.i("Status", "Disconnected");
+        }
+        else if (client.isConnected() == false){
+            TextView statusTxt = (TextView) findViewById(R.id.StatusLabel);
+            statusTxt.setText("Status: Connected");
+            Log.i("Status", "Connected");
+        }
 
         PraatButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,18 +106,30 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         switch (requestCode) {
             case RESULT_SPEECH: {
                 if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> text = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     GesprokenZin.setText(text.get(0));
                 }
                 break;
             }
         }
     }
+    public void SendMessage(View view) throws MqttException {
+        int qos = 2;
+        String content      = "llo";
+        MqttMessage message = new MqttMessage(content.getBytes());
+        message.setQos(qos);
+        message.setRetained(false);
+        client.publish("ha", message);
+    }
+
+
 }
